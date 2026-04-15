92950a265e7f0acd465187beb9854814553b3502
Norman <ubotirov77@gmail.com>
2026-04-16 01:22:16 +0900

    fix:learn keyword & default arguments

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@
 (1) Define vs Call
 (2) Parametr vs Argument
 (3) Keyword & default arguments
+(4) Scope
 
 '''
 
@@ -28,3 +29,19 @@ print("result:", result)
 
 result2 = greeting("Justin")
 print("result2:", result2)
+
+print("=====  Keyword & default arguments =====")
+
+
+# define
+def give_greet(name, age=24):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old"
+
+
+# call
+result3 = give_greet(name="Norman", age=23)
+print("result3:", result3)
+
+result4 = give_greet(name="Daniel")
+print("result3:", result4)
