3a3be431426b1fe712adfed82042d01c3f0196f8
Norman <ubotirov77@gmail.com>
2026-04-15 23:40:04 +0900

    fear: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
-print("Hello world from MIT43")
-print("this is my first git practice")
+# Dunder __builtins__ , __init__
+message = "Python: Everything is Object!"
+print(message)
+
+result = type(message)
+print("result:", result)
+
+''' In Python , there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTIONS > print() len() input() type() str() int()
+(3) CONSTANTS > True False None  
+'''
+
+print(dir(__builtins__))
