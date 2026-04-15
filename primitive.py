2956211517c889d447546664d00887e0a4275e6f
Norman <ubotirov77@gmail.com>
2026-04-16 00:25:33 +0900

    feat: learn string in pyhton

--- primitive.py
+++ primitive.py
@@ -9,3 +9,22 @@ print(f"the count{count} and type of count: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("===== string ======")
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the type of course:{result}")
+
+result = course.title()
+print(f"the result 1: {result}")
+
+result = course.upper()
+print(f"the result 2: {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result 2: {result}")
+print(course)
+
+print("===== boolean ======")
