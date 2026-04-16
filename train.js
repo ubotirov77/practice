a06aa968c982fbb4f59def71695494154e3803c6
Norman <ubotirov77@gmail.com>
2026-04-16 11:12:40 +0900

    fix: adding task explanatory

--- train.js
+++ train.js
@@ -1,5 +1,13 @@
 console.log("====== MIT TASK A with node.js ========")
-// MIT Task A
+
+/* ✨A-TASK:
+Shunday 2 parametrli function tuzing, 
+hamda birinchi parametrdagi letterni ikkinchi parametrdagi 
+so'zdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+
+// ✨ MASALANI YECHIMI:
 let letters = 0;
 let countedLetter = 0;
 
