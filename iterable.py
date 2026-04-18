e432c9b18303f472ecea6c55e7afb396c0218e54
Norman <ubotirov77@gmail.com>
2026-04-19 00:01:33 +0900

    feat : learn iterables in python

---
+++ iterable.py
@@ -0,0 +1,11 @@
+print("===== Iterable objrcts and range ======")
+# Iterable objects > string dict turple list range map filter
+
+range_obj = range(3)
+print("range_obj:", range_obj)
+
+for letter in "MIT":
+    print(f"the letter: {letter}")
+
+for ele in range_obj:
+    print(f"the element : {ele}")
