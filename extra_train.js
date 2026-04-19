ea468a1fa2a55d4ca130291d336f1647488d1c9a
Norman <ubotirov77@gmail.com>
2026-04-19 16:04:33 +0900

    feat: extra js task done

---
+++ extra_train.js
@@ -0,0 +1,29 @@
+const animal_list = ["fox", "ant", "bird", "lion", "lion", "wolf", "deer", "frog", "hen",
+    "mole", "duck", "goat", "dog", "cat", "bat", "cock", "cow"];
+
+function findAnimal(text) {
+    let count = 0
+    let findAnimal = []
+    for (let animal = 0; animal < animal_list.length; animal++) {
+
+        for (let letter = 0; letter < animal_list[animal].length; letter++) {
+            for (let data = 0; data < text.length; data++) {
+                if (text.length >= animal_list[animal].length) {
+                    if (text[data] === animal_list[animal][letter]) {
+                        count++
+
+                        if (count === animal_list[animal].length) {
+
+                            findAnimal.push(animal_list[animal])
+                            count = 0
+                        }
+                    }
+                }
+            }
+        }
+        count = 0
+    }
+    return findAnimal;
+}
+const javob = findAnimal("aodtcg");
+console.log("javob=>", javob);
