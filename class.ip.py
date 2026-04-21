cbc033d6bf91842ef441da24ee5baf0f9bd1ed8b
Norman <ubotirov77@gmail.com>
2026-04-22 01:43:05 +0900

    feat: Inheritence concept

---
+++ class.ip.py
@@ -0,0 +1,80 @@
+''' CLASS deep diving
+(1) Encapsulation
+(2) Inheriteance
+(3) Polymorphism
+'''
+print("==== Inheritence ====")
+# Parent > child
+# Parent only inherit its public and protected properties to children
+
+
+class Animal:
+
+    description = f"This class parent for animals"
+
+    def __init__(self, voice):
+        self.voice = voice
+        self._status = "the  animal is alive"
+
+    def make_voice(self):
+        print(f"the animal can make voice:{self.voice}")
+
+
+class Dog(Animal):
+
+    def __init__(self, name, sound, voice):
+        super().__init__(voice)
+        self.name = name
+        self.sound = sound
+
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}{self.sound}")
+
+    def protect(self):
+        print("yes I can protect you", self.sound, self.sound)
+
+
+class Cat(Animal):
+
+    def __init__(self, name, sound, voice):
+        super().__init__(voice)
+        self.name = name
+        self.sound = sound
+
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}{self.sound}")
+
+    def play(self):
+        print(f"I am {self.name} I love playing {self.sound}")
+
+
+class Fish(Animal):
+
+    def __init__(self, name, sound, voice):
+        super().__init__(voice)
+        self.name = name
+        self.sound = sound
+
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}{self.sound}")
+
+    def seek(self):
+        print(
+            f"men dadamni qidirib yuribman ko'rdadingizmi {self.sound} {self.sound}")
+
+
+dog = Dog("Rex🦮", "woof🔊", True)
+cat = Cat("Tom🐈", "meow🔊", True)
+fish = Fish("Nemo🐟", "💤", False)
+dog.introduce()
+cat.introduce()
+fish.introduce()
+
+print("------------------")
+dog.make_voice()
+fish.make_voice()
+print("------")
+print(Dog.description)
+print("status:", dog._status)
+print("status:", cat._status)
+print("status:", fish._status)
