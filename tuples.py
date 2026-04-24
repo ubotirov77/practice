1ec7b40141084bd37d50f455445c9eb24afd9ec6
Norman <ubotirov77@gmail.com>
2026-04-24 15:34:53 +0900

    fix: modify usage of *args/tuple and ** kwargs/dict together

--- tuples.py
+++ tuples.py
@@ -71,3 +71,11 @@ def introduce(**kwargs):
 # call
 introduce(name="Norman", age=23)
 introduce(name="Shawn", age=32, single=True)
+
+
+def greeting(*args, **kwargs):
+    print("*args", args)
+    print("**kwargs", kwargs)
+
+# call
+greeting("hi",True, 10 , name="John", age=22)
