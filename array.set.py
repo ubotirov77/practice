250ed71f14e46488f0acc6e1667044e14b87f5c8
Norman <ubotirov77@gmail.com>
2026-04-27 08:33:50 +0900

    fix: learn specific operators with set

--- array.set.py
+++ array.set.py
@@ -18,3 +18,30 @@ print("numbers(3)", numbers)
 
 del numbers[0:2]
 print("numbers(4)", numbers)
+
+print("===== Set ======")
+# set of unique collections without keeping order!
+new_numbers = array("i", [1, 4, 7, 5, 7, 5, 4, 7, 8, 4, 41])
+numb_set = set(new_numbers)
+print(f"the numb_set {numb_set} and type: {type(numb_set)}")
+
+numb_set.add(200)
+print("numb_set(1)", numb_set)
+
+numb_set.add(7)
+print("numb_set(2)", numb_set)
+
+print("===== Specific operators with set ======")
+# | & - ^
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b  # union
+result2 = a & b  # intersection
+result3 = a - b  # difference
+result4 = a ^ b  # symmetric difference
+
+print("result1:", result1)
+print("result2:", result2)
+print("result3:", result3)
+print("result4:", result4)
