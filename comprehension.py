81ce76e5cad3d5e4eb4c127b9c8bce8c6aaae19a
Norman <ubotirov77@gmail.com>
2026-04-29 04:13:21 +0900

    feat: set & dictionary comprehension

--- comprehension.py
+++ comprehension.py
@@ -34,3 +34,14 @@ cars = [
 ]
 list_cars = [car[0] for car in cars if car[1] > 80]  # c version
 print("list_cars", list_cars)
+
+print("===== set and dictionary comp =====")
+numbs = [1, 5, 4, 20, 4, 5, 1, 4]
+set_numbs = {*numbs}
+print("set_numbs:", set_numbs)
+
+dict_people = {person[0]: person[1]for person in people}
+dict_people = {person[0]: person[1]
+               for person in people if person[1] > 24}  # b version
+print("dict_people:", dict_people)
+
