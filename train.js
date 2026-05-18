39a5011d2089011943bad9c98a22de940b6e0b5d
Norman <ubotirov77@gmail.com>
2026-05-18 17:51:08 +0900

    feat: solution for MIT N- TASK

--- train.js
+++ train.js
@@ -1,26 +1,66 @@
 /*//================================= MIT TASK with node.js ==============================================
 //
+//                                       N-TASK (NodeJS)
+//=====================================================================================================
+ Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin
+ va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan 
+ object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin. MASALAN: 
+ getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+//===================================================================================================== */
+
+// ✨ MASALANI YECHIMI:
+function getSquareNumbers(arr) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push({
+            number: arr[i],
+            square: arr[i] * arr[i]
+        });
+    }
+
+    return result;
+}
+console.log(getSquareNumbers([10, 21, 4]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//================================= MIT TASK with node.js ==============================================
+//
 //                                 L-TASK (NodeJS)
 //=====================================================================================================
-    Shunday function yozing, u string qabul qilsin va string ichidagi hamma 
+    Shunday function yozing, u string qabul qilsin va string ichidagi hamma
     sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
     MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
 //===================================================================================================== */
 
-// ✨ MASALANI YECHIMI:
-function reverseSentence(str) {
-    let result = [];
-    let arr = str.split(" ");
+// // ✨ MASALANI YECHIMI:
+// function reverseSentence(str) {
+//     let result = [];
+//     let arr = str.split(" ");
 
-    for (let i = 0; i < arr.length; i++) {
-        let reversed = arr[i].split("").reverse().join("");
-        result.push(reversed);
-    }
+//     for (let i = 0; i < arr.length; i++) {
+//         let reversed = arr[i].split("").reverse().join("");
+//         result.push(reversed);
+//     }
 
-    return result.join(" ");
-}
+//     return result.join(" ");
+// }
 
-console.log(reverseSentence("Norman loves coding!"));
+// console.log(reverseSentence("Norman loves coding!"));
 
 
 
